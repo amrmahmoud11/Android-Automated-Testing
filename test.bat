3ced97b07b57dd89fed7f90fbc918a19e1da17c6
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 21:48:32 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,4 @@
 @echo off
 bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/mutetab-0.0.2-fx.xpi  C:\Users\Administrator\Desktop\photo.xpi
+cd "C:\Program Files (x86)\Mozilla Firefox"
+firefox.exe -ProfileManager -install-global-extension " C:\Users\Administrator\Desktop\photo.xpi"
