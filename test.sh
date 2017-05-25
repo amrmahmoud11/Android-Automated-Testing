1300906e0b48c43332b4c514febbded05a8922d9
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 08:47:46 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+sudo wget -O /usr/local/sbin/install-mozilla-addon http://bernaerts.dyndns.org/download/ubuntu/install-mozilla-addon
+sudo chmod +x /usr/local/sbin/install-mozilla-addon
+
+sudo install-mozilla-addon https://addons.mozilla.org/firefox/downloads/latest/5369/addon-5369-latest.xpi
+sudo install-mozilla-addon https://addons.mozilla.org/firefox/downloads/latest/1843/addon-1843-latest.xpi
