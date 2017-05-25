2a15463233da53c7b780ea2b8297fe2cde362250
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 23:06:50 +0200

    Update my.bat

--- my.bat
+++ my.bat
@@ -1,4 +1,4 @@
 @echo off
 
-bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/CleanVersion.crx  C:\Users\Administrator\Desktop\file.crx
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/mutetab-0.0.2-fx.xpi  C:\Users\Administrator\Desktop\mutetab-0.0.2-fx.xpi
 
