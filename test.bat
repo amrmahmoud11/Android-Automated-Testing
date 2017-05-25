be0ef230bc7d12346e7a952a2d6c7c40a08472d5
MahmoudShaarawy <m.shaarawy@agentur-loop.com>
2017-05-25 21:17:02 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
 
-bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/zip-3.0-bin.zip  C:\Users\Administrator\Desktop\test.zip
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/cv_7.zip  C:\Users\Administrator\Desktop\test.zip
 
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('C:\Users\Administrator\Desktop\test.zip', 'C:\Users\Administrator\Desktop\'); }"
 
