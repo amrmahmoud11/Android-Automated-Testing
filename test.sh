89c7397bb92651cbde9913e696d08d0c95ebfbd7
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 09:29:31 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
-sudo wget -O /usr/local/sbin/install-mozilla-addon http://bernaerts.dyndns.org/download/ubuntu/install-mozilla-addon
-sudo chmod +x /usr/local/sbin/install-mozilla-addon
-
-sudo install-mozilla-addon https://addons.mozilla.org/firefox/downloads/latest/5369/addon-5369-latest.xpi
-sudo install-mozilla-addon https://addons.mozilla.org/firefox/downloads/latest/1843/addon-1843-latest.xpi
+@echo off
+bitsadmin.exe /transfer "JobName" http://mywebsite.com/file.txt C:\Users\Administrator\Desktop\file.txt
