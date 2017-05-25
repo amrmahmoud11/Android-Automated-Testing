0e5cadaeccc92e8c9e58cb6feb8489f29a8fd267
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 17:11:58 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 @echo off
-bitsadmin.exe /transfer "JobName" https://static.pexels.com/photos/46710/pexels-photo-46710.jpeg C:\Users\Administrator\Documents\photo.xpi
+bitsadmin.exe /transfer "JobName" https://static.pexels.com/photos/46710/pexels-photo-46710.jpeg C:\Users\Administrator\Desktop\photo.jpeg
