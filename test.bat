9e225e87d045cd4b4097c77b75d5d0e93ed9929c
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 11:25:12 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 @echo off
-bitsadmin.exe /transfer "JobName" https://github.com/mosaadm/Android-Automated-Testing/edit/master/pom.xml C:\Users\Administrator\Desktop\pom.xml
+bitsadmin.exe /transfer "JobName" https://github.com/mosaadm/Android-Automated-Testing/edit/master/mutetab-0.0.2-fx.xpi C:\Users\Administrator\Desktop\mutetab-0.0.2-fx.xpi
