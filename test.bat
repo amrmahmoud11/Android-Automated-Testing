bb014b70026e2634ed292317150953ac1bb593d6
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 23:09:02 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,6 +1,2 @@
 @echo off
-
-bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/cv_7.zip  C:\Users\Administrator\Desktop\test.zip
-
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('C:\Users\Administrator\Desktop\test.zip', 'C:\Users\Administrator\Desktop\'); }"
-
+ bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/mutetab-0.0.2-fx.xpi  C:\Users\Administrator\Desktop\photo.xpi
