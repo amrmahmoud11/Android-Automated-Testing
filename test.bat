506fd759cb4df838a3559f8373a9cd9371d9dff4
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-25 21:14:51 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 @echo off
-bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/my.bat C:\Users\Administrator\Desktop\photo.xpi
+bitsadmin.exe /transfer /Download "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/mutetab-0.0.2-fx.xpi  C:\Users\Administrator\Desktop\photo.xpi
