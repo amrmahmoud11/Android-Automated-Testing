e4e65524880c6c1bf256c48aff2ae7ef939ecdc8
MahmoudShaarawy <m.shaarawy@agentur-loop.com>
2017-05-26 10:15:23 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,22 +1,4 @@
 @echo off
 bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/cv_7.zip  C:\Users\Administrator\Desktop\test.zip
-
-setlocal
-cd /d %~dp0
-Call :UnZipFile " C:\Users\Administrator\Desktop\" "C:\Users\Administrator\Desktop\test.zip"
-exit /b
-
-:UnZipFile <ExtractTo> <newzipfile>
-set vbs="%temp%\_.vbs"
-if exist %vbs% del /f /q %vbs%
->%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
->>%vbs% echo If NOT fso.FolderExists(%1) Then
->>%vbs% echo fso.CreateFolder(%1)
->>%vbs% echo End If
->>%vbs% echo set objShell = CreateObject("Shell.Application")
->>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
->>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
->>%vbs% echo Set fso = Nothing
->>%vbs% echo Set objShell = Nothing
-cscript //nologo %vbs%
-if exist %vbs% del /f /q %vbs%
+cd "C:\Users\Administrator\Desktop\"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('test.zip', 'bar'); }"
