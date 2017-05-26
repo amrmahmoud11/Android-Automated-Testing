f37ef683e011509d9483830320b6a0e9de3153b4
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-26 11:07:41 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,2 @@
 @echo off
- bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/cv_7.zip  C:\Users\Administrator\Desktop\test.zip
-
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/cv_7.zip  C:\Users\Administrator\Desktop\test.zip
