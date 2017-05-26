130266e3fdac60afed8c387070c414f032e2149e
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-26 18:05:39 +0200

    Update my.bat

--- my.bat
+++ my.bat
@@ -1,4 +1,3 @@
-@echo off
 bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/mutetab-0.0.2-fx.xpi  C:\Users\Administrator\Desktop\mutetab-0.0.2-fx.xpi
-cd "D:\Program Files\Mozilla Firefox\"
+cd "C:\Program Files (x86)\Mozilla Firefox\"
 firefox.exe -ProfileManager -install-global-extension "C:\Users\Administrator\Desktop\mutetab-0.0.2-fx.xpi"
