9155bce67ec268c5a87f69181443677365a24e7a
MahmoudShaarawy <m.shaarawy@agentur-loop.com>
2017-05-26 10:20:53 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 @echo off
-bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/cv_7.zip  C:\Users\Administrator\Desktop\test.zip
+bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/chrome_extension.zip  C:\Users\Administrator\Desktop\chrome_extension.zip
 cd "C:\Users\Administrator\Desktop\"
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('test.zip', 'bar'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('chrome_extension.zip', ''); }"
