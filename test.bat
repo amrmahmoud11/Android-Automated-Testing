dd9e332f0cec6a13341544f8b660197fcee07892
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-05-26 18:37:06 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,4 +2,4 @@
 bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Android-Automated-Testing/master/chrome_extension.zip  C:\Users\Administrator\Desktop\chrome_extension.zip
 cd "C:\Users\Administrator\Desktop\"
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('chrome_extension.zip', 'chrome'); }"
-"D:\Program Files\Chrome 58\58.0.3029.81\chrome.exe" --load-extension=C:\Users\Administrator\Desktop\chrome\chrome_extension
+echo "D:\Program Files\Chrome 58\58.0.3029.81\chrome.exe" --load-extension=C:\Users\Administrator\Desktop\chrome\chrome_extension
